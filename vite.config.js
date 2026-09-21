e30f76fe0ce8db2a2aff4ca1630480a5b1442198
Rudy Sam <rudysam4844@gmail.com>
2026-09-21 13:15:00 +0500

    ok

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    host: 'localhost',
     port: 5173,
-    host: true, // Listen on all local IP addresses (0.0.0.0) so mobile devices on Wi-Fi can connect
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
